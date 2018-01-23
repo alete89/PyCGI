55fae329861ebea6f4ed719ccf46faf545c39907
alete89 <maildealete@gmail.com>
2018-01-23 19:18:44 -0300

    probando otra forma de llamar procesos en orden

--- process.py
+++ process.py
@@ -21,12 +21,16 @@ class Process():
         self.processRun.readyRead.connect(self.getOutput)
 
     def EjecutarComandos(self, comandos, parametros, iteraciones):
+        #prueba
+        self.lista = map(list, zip(comandos,parametros,iteraciones))
+        #prueba
+        return
         for c, comando in enumerate(comandos):
             parametro = parametros[c]
             iteracion = iteraciones[c]
             for _ in range(int(iteracion)):
-                print comando, parametro
-                self.processRun.waitForFinished()
+                #print comando, parametro
+                self.processRun.waitForFinished() # Necesario para la ejecución en secuencia. Friza el mainwindow
                 if not parametro:
                     self.processRun.start(comando)
                 else:
@@ -40,3 +44,14 @@ class Process():
             self.mainWindow.showOutputInTerminal(salida)
         else:
             self.mainWindow.showOutputInTerminal(error)
+
+    def runNow (self, cmd, param, iter):
+        self.process = QtCore.QProcess(self)
+        self.process.finished.connect(self.termino)
+        self.process.readyRead.connect(self.hayParaEscribir)
+
+    def termino(self):
+        pass
+
+    def hayParaEscribir(self):
+        pass

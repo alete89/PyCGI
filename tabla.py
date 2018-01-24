91c6870f6860e0359427b20ec467a5166b65940b
alete <4999277+alete89@users.noreply.github.com>
2018-01-24 16:23:01 -0300

    agregada la clase Tabla

---
+++ tabla.py
@@ -0,0 +1,20 @@
+from PyQt4 import QtGui
+
+
+class Tabla(QtGui.QTableWidget):
+    def __init__(self):
+        super(Tabla, self).__init__()
+        self.dataset = []
+
+    def ShowDataSet(self, dataset, header=[]):
+        self.dataset = dataset
+        self.setRowCount(0)
+        self.setColumnCount(0)
+        for rowdata in self.dataset:
+            row = self.rowCount()
+            self.insertRow(row)
+            self.setColumnCount(len(rowdata))
+            for column, data in enumerate(rowdata):
+                item = QtGui.QTableWidgetItem(data.decode('utf8'))
+                self.setItem(row, column, item)
+        self.setHorizontalHeaderLabels(header)

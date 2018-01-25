28c7907494daad035df1b4a054acb116aeab64aa
alete89 <maildealete@gmail.com>
2018-01-25 01:57:07 -0300

    eliminados los espacios incorrectos en KeywordPatterns

--- Highlighter.py
+++ Highlighter.py
@@ -14,10 +14,10 @@ class Highlighter(QtGui.QSyntaxHighlighter):
         keywordFormat.setForeground(QtCore.Qt.darkBlue)
         keywordFormat.setFontWeight(QtGui.QFont.Bold)
 
-        keywordPatterns = [' and ', 'assert', 'break', 'class', 'continue', ' def ',
-                           ' del ', 'elif', 'else', 'except', 'exec', 'finally',
-                           ' for ', 'from', 'global', ' if ', 'import', ' in ',
-                           ' is ', 'lambda', 'not', ' or ', 'pass', 'print',
+        keywordPatterns = ['and', 'assert', 'break', 'class', 'continue', 'def',
+                           'del', 'elif', 'else', 'except', 'exec', 'finally',
+                           'for', 'from', 'global', 'if', 'import', 'in',
+                           'is', 'lambda', 'not', 'or', 'pass', 'print',
                            'raise', 'return', 'try', 'while', 'yield',
                            'None', 'True', 'False', 'self', ]
 

75e11139f7bcf4ef036b9d93fe15a24062d038e2
alete <4999277+alete89@users.noreply.github.com>
2018-02-09 14:42:07 -0300

    refactor y PEP8

--- ProcessClass.py
+++ ProcessClass.py
@@ -2,48 +2,49 @@
 
 # Default
 import sys
-# no entiendo por qué no funciona importando ctypes directamente.
-# Windows Only:
+from PyQt4 import QtCore
+
 if sys.platform == "win32":
+    # Windows only
     from winstructs import WinProcInfo
     import ctypes
-# GUI
-import PyQt4.QtCore
 
 
 class Process():
     def __init__(self):
-        self.proc = PyQt4.QtCore.QProcess()
-        self.proc.setProcessChannelMode(PyQt4.QtCore.QProcess.MergedChannels)
-        self.proc.setReadChannelMode(PyQt4.QtCore.QProcess.MergedChannels)
+        self.proc = QtCore.QProcess()
+        self.proc.setProcessChannelMode(QtCore.QProcess.MergedChannels)
+        self.proc.setReadChannelMode(QtCore.QProcess.MergedChannels)
         self.proc.finished.connect(self.processJustFinished)
         self.proc.readyRead.connect(self.hayParaEscribir)
 
-        self.lista = []
+        self.secuencia = []
         self.MainWindowInstance = None
         self.current_process = ""
 
-    def EjecutarComandos(self, comandos, parametros, iteraciones, mw):
+    def ejecutarSecuencia(self, comandos, parametros, iteraciones, mw):
         self.MainWindowInstance = mw
-        self.lista = map(list, zip(comandos, parametros, iteraciones))
+        self.secuencia = map(list, zip(comandos, parametros, iteraciones))
         self.runNow()
 
     def runNow(self):
-        if (len(self.lista) == 0):
+        if not self.secuencia:
             return
-        sublist = self.lista[0]
-        if int(sublist[2]) == 0:
-            del self.lista[0]
+        instruccion = self.secuencia[0]
+        if int(instruccion[2]) == 0:  # Iteraciones restantes
+            # Si no hay más iteraciones, la saco de la lista.
+            del self.secuencia[0]
             self.runNow()
-        else:
-            sublist[2] = str(int(sublist[2]) - 1)
-            self.current_process = sublist[0]
+        else:  # Quedan iteraciones
+            instruccion[2] = str(int(instruccion[2]) - 1)  # Iteraciones -1
+            self.current_process = instruccion[0]
             self.MainWindowInstance.showOutputInTerminal(
                 "iniciando proceso: " + self.current_process)
-            if not sublist[1]:
-                self.proc.start(sublist[0])
+            if not instruccion[1]:  # Si no hay parámetros
+                self.proc.start(instruccion[0])  # lanzo sin parámetros
             else:
-                self.proc.start(sublist[0], sublist[1])
+                # lanzo con parámetros
+                self.proc.start(instruccion[0], instruccion[1])
 
     def hayParaEscribir(self):
         output = self.proc.readAll().data()
@@ -58,8 +59,8 @@ class Process():
             else:
                 pid = int(self.proc.pid())
             return pid
-        except UserWarning:
-            pass  # VER
+        except TypeError:
+            return "No hay proceso corriendo"
 
     def killCurrentProcess(self, mw):
         mw.showOutputInTerminal(str(self.getPid()))

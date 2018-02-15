97d9318b1db156c1ecf6e4f582c9351ed1f12b15
alete89 <maildealete@gmail.com>
2018-02-15 15:35:13 -0300

    uso una tabla estática para los tests

--- test/test_csvdb.py
+++ test/test_csvdb.py
@@ -1,13 +1,13 @@
+import os
 import unittest
 import src.logic.csvdb as csvdb
-import src.logic.core as core
-import os
 
 
 class TestCore(unittest.TestCase):
     def setUp(self):
+        self.path = os.getcwd() + "/test/tabla_test.csv"
         self.testcsv = "test.csv"
-        self.dataset = [
+        self.test_dataset = [
             ['1', 'archivo', 'abrir', '2', '1', 'python ./scripts/abrir.py', '1'],
             ['2', 'edicion', 'copiar', '2', '1',
                 'python ./scripts/copiar.py', '5'],
@@ -23,15 +23,6 @@ class TestCore(unittest.TestCase):
             ['10', 'prueba', 'numeros', '3', '4', 'python ./scripts/tres.py', '1'],
             ['11', 'prueba', 'numeros', '4', '1', 'ping (ip)', '1']
         ]
-        self.columna = 1
-        self.valor = "archivo"
-        self.dataset_filtrado_expected = [
-            ['1', 'archivo', 'abrir', '2', '1', 'python ./scripts/abrir.py', '1'],
-            ['4', 'archivo', 'guardar como', '1', '1',
-                'python ./scripts/guardar.py', '1']
-        ]
-        self.header = ["id", "menu", "submenu", "posicion en menu",
-                       "orden en secuencia", "comando", "loop"]
 
     def tearDown(self):
         try:
@@ -40,22 +31,45 @@ class TestCore(unittest.TestCase):
             pass
 
     def test_dataFilter(self):
-        self.assertEqual(csvdb.dataFilter(self.dataset, self.columna,
-                                          self.valor), self.dataset_filtrado_expected)
+        columna = 1
+        valor = "archivo"
+        dataset_filtrado_expected = [
+            ['1', 'archivo', 'abrir', '2', '1', 'python ./scripts/abrir.py', '1'],
+            ['4', 'archivo', 'guardar como', '1', '1',
+                'python ./scripts/guardar.py', '1']
+        ]
+        self.assertEqual(csvdb.dataFilter(self.test_dataset, columna,
+                                          valor), dataset_filtrado_expected)
 
     def test_getDataFromCsv(self):
         #self.maxDiff = 0
-        self.assertEqual(csvdb.getDataFromCsv(core.default_path), self.dataset)
+        self.assertEqual(csvdb.getDataFromCsv(
+            self.path), self.test_dataset)
 
     def test_getHeader(self):
-        self.assertEqual(csvdb.getHeader(core.default_path), self.header)
+        header_expected = ["id", "menu", "submenu", "posicion en menu",
+                           "orden en secuencia", "comando", "loop"]
+        self.assertEqual(csvdb.getHeader(self.path), header_expected)
 
     def test_saveCSV(self):
-        self.dataset.append(
+        self.test_dataset.append(
             ["cero", "uno", "dos", "tres", "cuatro", "cinco", "seis"])
-        csvdb.SaveCSV("test.csv", self.dataset,
-                      csvdb.getHeader(core.default_path))
-        self.assertEqual(self.dataset, csvdb.getDataFromCsv(self.testcsv))
+        csvdb.SaveCSV("test.csv", self.test_dataset,
+                      csvdb.getHeader(self.path))
+        self.assertEqual(self.test_dataset, csvdb.getDataFromCsv(self.testcsv))
+
+    def test_distinct(self):
+        columna = 1
+        distinct_expected = [
+            ['1', 'archivo', 'abrir', '2', '1', 'python ./scripts/abrir.py', '1'],
+            ['2', 'edicion', 'copiar', '2', '1',
+                'python ./scripts/copiar.py', '5'],
+            ['3', 'ver', 'ping', '1', '2', 'ping (ingrese una IP)', '2'],
+            ['7', 'Dibujo', 'Paint', '1', '1', 'mspaint', '2'],
+            ['8', 'prueba', 'numeros', '1', '2', 'python ./scripts/uno.py', '1']
+        ]
+        self.assertEqual(csvdb.distinct(
+            self.test_dataset, columna), distinct_expected)
 
 
 if __name__ == '__main__':

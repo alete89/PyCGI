0c8d3168197454e7db685a459b849ff2b729d563
alete <4999277+alete89@users.noreply.github.com>
2018-05-02 16:57:46 -0300

    menu contextual en el treeView (sin acciones)

--- src/gui/mainWindow.py
+++ src/gui/mainWindow.py
@@ -13,7 +13,8 @@ class PyCGI(QtGui.QMainWindow):
         self.is_new = True  # Flag para el archivo del editor
         self.file_name = ''  # Nombre del archivo del editor
 
-        self.setWindowTitle('PyCGI - Instituto de Tecnologia Nuclear Dan Beninson')
+        self.setWindowTitle(
+            'PyCGI - Instituto de Tecnologia Nuclear Dan Beninson')
 
         self.treeWidget()
         self.crearIndicadorSecuencia()
@@ -29,12 +30,37 @@ class PyCGI(QtGui.QMainWindow):
         self.model.setRootPath(QtCore.QDir.currentPath())
 
         self.tree = QtGui.QTreeView()
+        self.tree.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.tree.customContextMenuRequested.connect(self.openContextMenu)
+
         self.tree.setModel(self.model)
         self.tree.setAnimated(True)
         self.tree.setIndentation(15)
         self.tree.setSortingEnabled(True)
         self.tree.setColumnWidth(0, 300)
         self.tree.doubleClicked.connect(self.openFileFromTree)
+
+    def openContextMenu(self, position):
+        indexes = self.tree.selectedIndexes()
+        # El if a continuación obtiene el nivel de profundidad del elemento
+
+        if len(indexes) > 0:
+            level = 0
+            index = indexes[0]
+            selected = indexes[0]
+            while index.parent().isValid():
+                index = index.parent()
+                level += 1
+
+        rightClickMenu = QtGui.QMenu()
+        # print self.model.fileName(selected)
+        # print self.model.filePath(selected)
+        if self.model.fileName(selected)[-3:] == '.py':
+            rightClickMenu.addAction(self.tr("Agregar a Menu"))
+        rightClickMenu.addAction(
+            self.tr("Copy Path" + str(self.model.filePath(selected))))
+
+        rightClickMenu.exec_(self.tree.viewport().mapToGlobal(position))
 
     def crearIndicadorSecuencia(self):
         self.font = QtGui.QFont()
@@ -44,13 +70,15 @@ class PyCGI(QtGui.QMainWindow):
         self.indicadorSecuencia.setReadOnly(True)
         self.indicadorSecuencia.setFont(self.font)
         self.indicadorSecuencia.setMaximumHeight(50)
-        self.indicadorSecuencia.setFontWeight(75)  # BOLD sólo para la instrucción actual!
+        # BOLD sólo para la instrucción actual!
+        self.indicadorSecuencia.setFontWeight(75)
 
     def crearTerminal(self):
         self.terminalOutput = QtGui.QTextEdit(self)
         self.terminalOutput.setReadOnly(True)
         self.terminalOutput.setFont(self.font)
-        self.terminalOutput.setStyleSheet("background-color: #585858; color: #fff")
+        self.terminalOutput.setStyleSheet(
+            "background-color: #585858; color: #fff")
 
         self.cursor = self.terminalOutput.textCursor()
 
@@ -61,7 +89,8 @@ class PyCGI(QtGui.QMainWindow):
         self.EditorDeTexto.setStyleSheet("background-color: #f1f1f1;")
         self.EditorDeTexto.setMinimumHeight(100)
 
-        self.pythonHighlighter = Highlighter.Highlighter(self.EditorDeTexto.document())
+        self.pythonHighlighter = Highlighter.Highlighter(
+            self.EditorDeTexto.document())
 
     def crearToolbar(self):
         toolbar = self.addToolBar("Editor de texto Toolbar")
@@ -76,12 +105,14 @@ class PyCGI(QtGui.QMainWindow):
         SaveIcon.triggered.connect(self.saveDialog)
         toolbar.addAction(SaveIcon)
 
-        SaveAsIcon = QtGui.QAction(QtGui.QIcon('icons/saveAs.png'), 'Save as', self)
+        SaveAsIcon = QtGui.QAction(QtGui.QIcon(
+            'icons/saveAs.png'), 'Save as', self)
         SaveAsIcon.setShortcut('Ctrl+g')
         SaveAsIcon.triggered.connect(self.saveAsDialog)
         toolbar.addAction(SaveAsIcon)
 
-        CloseIcon = QtGui.QAction(QtGui.QIcon('icons/closeFile.png'), 'Close', self)
+        CloseIcon = QtGui.QAction(QtGui.QIcon(
+            'icons/closeFile.png'), 'Close', self)
         CloseIcon.setShortcut('Ctrl+x')
         CloseIcon.triggered.connect(self.CloseDialog)
         toolbar.addAction(CloseIcon)
@@ -169,7 +200,8 @@ class PyCGI(QtGui.QMainWindow):
             for subMenu, idFila in zip(core.subMenuList(menu, core.fullDataSet()), core.idList(menu, core.fullDataSet())):
                 action = QtGui.QAction('&' + str(subMenu), self)
                 action.setStatusTip(str(idFila) + " - " + str(subMenu))
-                action.triggered.connect(lambda ignore, idt=subMenu: self.subMenuOptionClicked(idt))
+                action.triggered.connect(
+                    lambda ignore, idt=subMenu: self.subMenuOptionClicked(idt))
                 thisMenu.addAction(action)
 
     def subMenuOptionClicked(self, subMenu):
@@ -221,7 +253,8 @@ class PyCGI(QtGui.QMainWindow):
         self.tabs.setCurrentWidget(self.tab2)
 
     def saveAsDialog(self):
-        name = QtGui.QFileDialog.getSaveFileName(self, 'Save File', str(self.file_name))
+        name = QtGui.QFileDialog.getSaveFileName(
+            self, 'Save File', str(self.file_name))
         if name:
             textoParaGuardar = self.EditorDeTexto.toPlainText()
             with open(name, "w") as nFile:
@@ -241,7 +274,8 @@ class PyCGI(QtGui.QMainWindow):
         if self.is_new:
             msg = QtGui.QMessageBox()
             msg.setIcon(QtGui.QMessageBox.Warning)
-            msg.setText(u"Se modificó el documento desde la última vez que se guardó")
+            msg.setText(
+                u"Se modificó el documento desde la última vez que se guardó")
             msg.setInformativeText("desea guardar los cambios?")
             msg.setStandardButtons(QtGui.QMessageBox.Save |
                                    QtGui.QMessageBox.Discard | QtGui.QMessageBox.Cancel)

c8ff8b5face457e64de6f7df3b139cea02d1b91f
Instituto Dan Beninson <manuel@localhost.localdomain>
2018-05-02 11:30:55 -0300

    agregue la linea nada

--- PyCGI.py
+++ PyCGI.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 import sys
 sys.path.append('c:/Users/Alejandro/Documents/Alejandro/Python/PyCGI/')
 
-
+# nada
 if __name__ == '__main__':
     import src.gui.mainWindow as mainWindow
     from PyQt4.QtGui import QApplication

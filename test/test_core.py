c2dffb0812015dec09bc69fe2ba31bfd8076f38a
alete89 <maildealete@gmail.com>
2018-05-10 14:00:03 -0300

    tests

--- test/test_core.py
+++ test/test_core.py
@@ -5,15 +5,20 @@ import os
 
 class TestCore(unittest.TestCase):
     def setUp(self):
-        self.path_esperado = 'PRUEBA'
-        print os.getcwd()
+        pass
 
     def tearDown(self):
         pass
 
-    def test_cfg_file(self):
+    def test_getTreeViewInitialPath_from_cfg(self):
+        path_esperado = 'InitialPath'
         core.CFG_PATH = os.getcwd() + "\\test\\cfg_test"
-        self.assertEqual(self.path_esperado, core.getTreeViewInitialPath())
+        self.assertEqual(path_esperado, core.getTreeViewInitialPath())
+
+    def test_getTreeViewRootPath_from_cfg(self):
+        path_esperado = "pathRoot"
+        core.CFG_PATH = os.getcwd() + "\\test\\cfg_test"
+        self.assertEqual(path_esperado, core.getTreeViewRootPath())
 
 
 if __name__ == '__main__':

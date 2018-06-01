aa6cd3ce9fac9c6c2f9d95d8c5e8fb07e7bb5bac
alete89 <maildealete@gmail.com>
2018-06-01 03:24:36 +0000

    test paramFinder

---
+++ test/test_paramFinder.py
@@ -0,0 +1,21 @@
+import unittest
+import src.logic.paramFinder as pf
+
+
+class TestCore(unittest.TestCase):
+    def setUp(self):
+        self.test_dataset = [
+            ['3', 'ver', 'ping', '1', '2', 'ping (ingrese una IP)', '2']
+        ]
+
+    def tearDown(self):
+        pass
+
+    def test_paramFinder(self):
+        comandos, parametros = pf.findParameters(self.test_dataset)
+        self.assertEqual(comandos, ["ping"])
+        self.assertEqual(parametros, [["ingrese una IP"]])
+
+
+if __name__ == '__main__':
+    unittest.main()

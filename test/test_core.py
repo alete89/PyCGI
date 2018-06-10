673b18d367b12baf4e45747e161d3f446bd98325
alete89 <maildealete@gmail.com>
2018-06-10 00:06:31 -0300

    paths unix compatible

--- test/test_core.py
+++ test/test_core.py
@@ -12,12 +12,12 @@ class TestCore(unittest.TestCase):
 
     def test_getTreeViewInitialPath_from_cfg(self):
         path_esperado = 'InitialPath'
-        core.CFG_PATH = os.getcwd() + "\\test\\cfg_test"
+        core.CFG_PATH = os.getcwd() + "/test/cfg_test"
         self.assertEqual(path_esperado, core.getTreeViewInitialPath())
 
     def test_getTreeViewRootPath_from_cfg(self):
         path_esperado = "pathRoot"
-        core.CFG_PATH = os.getcwd() + "\\test\\cfg_test"
+        core.CFG_PATH = os.getcwd() + "/test/cfg_test"
         self.assertEqual(path_esperado, core.getTreeViewRootPath())
 
 
